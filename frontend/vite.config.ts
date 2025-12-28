62a23fe5b7351dc6eca9e1507c3d13d563a4fed1
C C <sakhong@amazon.com>
2025-12-27 22:00:18 -0700

    Update frontend to use production backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,12 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
-    },
+
   },
   preview: {
     allowedHosts: [
